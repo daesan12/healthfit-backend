bf93a2909e403a3ff96c419dc83630fc8646577f
장대산 <diqkraud@naver.com>
2026-06-19 04:44:30 +0900

    🚀import 추가 및 APP등록

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -37,6 +41,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 외부 라이브러리
+    'rest_framework',
+    'corsheaders',
+    'rest_framework_simplejwt.token_blacklist',
+
+    # 직접 만든 앱
+    'accounts',
+    'diets',
+    'workouts',
+    'ai_services',
+    'community',
 ]
 
 MIDDLEWARE = [
